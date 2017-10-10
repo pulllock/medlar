c0bdc1ed4603d01050f56d3c9c8d2b62eed7cba4
dachengxi <dachengdaxi@gmail.com>
2017-10-10 23:03:24 +0800

    FileOutputStream

---
+++ javatest/src/main/java/me/cxis/io/FileOutputStreamExample.java
@@ -0,0 +1,28 @@
+package me.cxis.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by cheng.xi on 2017-10-10 22:59.
+ */
+public class FileOutputStreamExample {
+    public static void main(String[] args) throws IOException {
+        OutputStream outputStream = new FileOutputStream("...");
+        while (true) {
+            int data = 111;
+            outputStream.write(data);
+        }
+
+        outputStream.close();
+
+        //
+
+        OutputStream outputStream1 = new FileOutputStream("xxx", true);
+        OutputStream outputStream2 = new FileOutputStream("xxx", false);
+
+
+    }
+}
