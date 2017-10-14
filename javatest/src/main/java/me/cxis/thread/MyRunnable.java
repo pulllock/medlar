cedb3fa912cbef2c905f765e489ffe84ea617028
dachengxi <dachengdaxi@gmail.com>
2017-10-14 22:00:32 +0800

    Thread

---
+++ javatest/src/main/java/me/cxis/thread/MyRunnable.java
@@ -0,0 +1,11 @@
+package me.cxis.thread;
+
+/**
+ * Created by cheng.xi on 2017-10-14 21:53.
+ */
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("my runnable running...");
+    }
+}
