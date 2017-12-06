86d13dbd111b51ca4c4ef5a79b106f68170077f2
Pullock <pullocked@gmail.com>
2017-12-06 16:40:08 +0800

    ArrayBlockingQueue

---
+++ javatest/src/main/java/me/cxis/blockingqueue/arrayblockingqueue/Message.java
@@ -0,0 +1,14 @@
+package me.cxis.blockingqueue.arrayblockingqueue;
+
+public class Message {
+
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
