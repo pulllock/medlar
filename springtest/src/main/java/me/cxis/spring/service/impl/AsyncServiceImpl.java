4cd38b55987f3ac0640fcf4f2de4167c9569c670
Pullock <pullocked@gmail.com>
2018-04-19 19:28:53 +0800

    Spring @Async1

--- springtest/src/main/java/me/cxis/spring/service/impl/AsyncServiceImpl.java
+++ springtest/src/main/java/me/cxis/spring/service/impl/AsyncServiceImpl.java
@@ -2,7 +2,11 @@ package me.cxis.spring.service.impl;
 
 import me.cxis.spring.service.AsyncService;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @Service
 public class AsyncServiceImpl implements AsyncService {
@@ -16,5 +20,30 @@ public class AsyncServiceImpl implements AsyncService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    @Async
+    public String testAsync1() {
+        Future<String> future = doSomething();
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return "xxx";
+    }
+
+    public Future<String> doSomething() {
+        try {
+            Thread.sleep(10000);
+            System.out.println(Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return new AsyncResult<>("xxxxxxx");
     }
 }
