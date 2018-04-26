694cab6b7266cdb67c5ec2b63964be7639b358dc
dachengxi <dachengdaxi@gmail.com>
2018-04-26 22:21:07 +0800

    second kill spring mybatis

---
+++ second-kill/src/main/java/me/cxis/second/kill/dao/SuccessKilledDao.java
@@ -0,0 +1,19 @@
+package me.cxis.second.kill.dao;
+
+import me.cxis.second.kill.dao.mapper.SuccessKilledMapper;
+import me.cxis.second.kill.dao.model.SuccessKilledDO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SuccessKilledDao {
+
+    private SuccessKilledMapper successKilledMapper;
+
+    public int insertSuccessKilled(long secondSkillId, long userPhone) {
+        return successKilledMapper.insertSuccessKilled(secondSkillId, userPhone);
+    }
+
+    public SuccessKilledDO queryBySecondKillId(long secondKillId) {
+        return successKilledMapper.queryBySecondKillId(secondKillId);
+    }
+}
