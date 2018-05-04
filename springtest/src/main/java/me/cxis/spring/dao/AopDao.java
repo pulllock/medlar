2cd7677138fd482aef7c02adcb26d591a237eda1
dachengxi <dachengdaxi@gmail.com>
2018-05-04 16:37:29 +0800

    aop custom annotation

--- springtest/src/main/java/me/cxis/spring/dao/AopDao.java
+++ springtest/src/main/java/me/cxis/spring/dao/AopDao.java
@@ -8,4 +8,13 @@ public class AopDao {
     public String getSomething() {
         return "something";
     }
+
+    public String getSomething1() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "something1";
+    }
 }
--- springtest/src/main/java/me/cxis/spring/service/AopService.java
+++ springtest/src/main/java/me/cxis/spring/service/AopService.java
@@ -3,4 +3,6 @@ package me.cxis.spring.service;
 public interface AopService {
 
     String calculateSomething();
+
+    String calculateSomething1();
 }
