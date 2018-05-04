26debfa29d2b17714feafcc517f3db01ca1a4793
dachengxi <dachengdaxi@gmail.com>
2018-05-04 16:23:00 +0800

    aop after

--- springtest/src/main/java/me/cxis/spring/aspect/UserAccessAspect.java
+++ springtest/src/main/java/me/cxis/spring/aspect/UserAccessAspect.java
@@ -1,6 +1,7 @@
 package me.cxis.spring.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -23,5 +24,10 @@ public class UserAccessAspect {
     @AfterReturning(value = "execution(* me.cxis.spring.service.*.*(..))", returning = "result")
     public void afterReturningForService(JoinPoint joinPoint, Object result) {
         logger.info("[{}] returned with value: {}", joinPoint, result);
+    }
+
+    @After("execution(* me.cxis.spring.service.*.*(..))")
+    public void afterForService(JoinPoint joinPoint) {
+        logger.info("after execution of {}", joinPoint);
     }
 }
