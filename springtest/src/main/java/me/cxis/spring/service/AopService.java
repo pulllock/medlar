7aab4eacae4261e0672b091f8ab7ee1346570380
Pullock <pullocked@gmail.com>
2018-05-04 16:17:49 +0800

    aop before and afterReturning

---
+++ springtest/src/main/java/me/cxis/spring/service/AopService.java
@@ -0,0 +1,6 @@
+package me.cxis.spring.service;
+
+public interface AopService {
+
+    String calculateSomething();
+}
---
+++ springtest/src/main/java/me/cxis/spring/service/impl/AopServiceImpl.java
@@ -0,0 +1,25 @@
+package me.cxis.spring.service.impl;
+
+import me.cxis.spring.dao.AopDao;
+import me.cxis.spring.service.AopService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service("aopService")
+public class AopServiceImpl implements AopService {
+
+    private Logger logger = LoggerFactory.getLogger(AopService.class);
+
+    @Resource
+    private AopDao aopDao;
+
+    @Override
+    public String calculateSomething() {
+        String value = aopDao.getSomething();
+        logger.info("In AopService - {}", value);
+        return value;
+    }
+}
