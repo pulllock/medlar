78cecc967472930b7008de7a7d07960b5a792894
Pullock <pullocked@gmail.com>
2018-05-07 20:37:15 +0800

    netty usage example fix frame data

--- netty-study/src/main/java/me/cxis/netty/example/time/TimeClientHandler.java
+++ netty-study/src/main/java/me/cxis/netty/example/time/TimeClientHandler.java
@@ -1,25 +1,36 @@
 package me.cxis.netty.example.time;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.concurrent.EventExecutorGroup;
 
 import java.util.Date;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
+    private ByteBuf byteBuf;
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        byteBuf = ctx.alloc().buffer(4);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        byteBuf.release();
+        byteBuf = null;
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf time = (ByteBuf) msg;
+        byteBuf.writeBytes(time);
+        time.release();
 
-        try {
+        if (byteBuf.readableBytes() >= 4) {
             long current = (time.readUnsignedInt() - 2208988800L) * 1000L;
             System.out.println(new Date(current));
             ctx.close();
-        } finally {
-            time.release();
         }
     }
 
