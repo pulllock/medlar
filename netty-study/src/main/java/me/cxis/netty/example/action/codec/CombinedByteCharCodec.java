ed26ca987f646b5fd56ec55fc40498546de6a2cf
Pullock <pullocked@gmail.com>
2018-05-10 17:01:36 +0800

    CombinedChannelDuplexHandler

---
+++ netty-study/src/main/java/me/cxis/netty/example/action/codec/CombinedByteCharCodec.java
@@ -0,0 +1,9 @@
+package me.cxis.netty.example.action.codec;
+
+import io.netty.channel.CombinedChannelDuplexHandler;
+
+public class CombinedByteCharCodec extends CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
+    public CombinedByteCharCodec() {
+        super(new ByteToCharDecoder(), new CharToByteEncoder());
+    }
+}
