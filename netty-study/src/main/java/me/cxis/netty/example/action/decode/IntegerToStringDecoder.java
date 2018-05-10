34d7c39ce910e6581ac36ac8547e78dbd9bb9759
Pullock <pullocked@gmail.com>
2018-05-10 15:38:29 +0800

    MessageToMessageDecoder

---
+++ netty-study/src/main/java/me/cxis/netty/example/action/decode/IntegerToStringDecoder.java
@@ -0,0 +1,13 @@
+package me.cxis.netty.example.action.decode;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.util.List;
+
+public class IntegerToStringDecoder extends MessageToMessageDecoder<Integer> {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, Integer msg, List<Object> out) throws Exception {
+        out.add(String.valueOf(msg));
+    }
+}
