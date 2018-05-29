67f96b22df9aec664d882a859e72f99fc3625c01
Pullock <pullocked@gmail.com>
2018-05-29 16:02:04 +0800

    jersey: restful api

---
+++ jersey-test/src/main/java/me/cxis/JerseyApplication.java
@@ -0,0 +1,9 @@
+package me.cxis;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("")
+public class JerseyApplication extends ResourceConfig {
+}
