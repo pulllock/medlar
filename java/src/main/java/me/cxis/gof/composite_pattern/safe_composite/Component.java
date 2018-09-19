7d968bbfd2eedf1c0a670ba6280fc09b19fbe677
Pullock <pullocked@gmail.com>
2018-09-19 14:29:15 +0800

    safe composite pattern

---
+++ java/src/main/java/me/cxis/gof/composite_pattern/safe_composite/Component.java
@@ -0,0 +1,6 @@
+package me.cxis.gof.composite_pattern.safe_composite;
+
+public abstract class Component {
+
+    public abstract void operation();
+}
---
+++ java/src/main/java/me/cxis/gof/composite_pattern/safe_composite/Leaf.java
@@ -0,0 +1,9 @@
+package me.cxis.gof.composite_pattern.safe_composite;
+
+public class Leaf extends Component {
+
+    @Override
+    public void operation() {
+
+    }
+}
