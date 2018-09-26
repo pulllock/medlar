17509d2267a6ad5e37281f8152caea06fb099a16
Pullock <pullocked@gmail.com>
2018-09-26 11:08:27 +0800

    decorator pattern

---
+++ java/src/main/java/me/cxis/gof/decorator_pattern/Decorator.java
@@ -0,0 +1,15 @@
+package me.cxis.gof.decorator_pattern;
+
+public class Decorator implements Component {
+
+    private Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public void operation() {
+        component.operation();
+    }
+}
