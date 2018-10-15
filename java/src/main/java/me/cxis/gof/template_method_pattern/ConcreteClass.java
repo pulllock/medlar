b38fc5f6615615dd6402461c66ab35b01eba20bd
Pullock <pullocked@gmail.com>
2018-10-15 16:56:12 +0800

    template method pattern

---
+++ java/src/main/java/me/cxis/gof/template_method_pattern/ConcreteClass.java
@@ -0,0 +1,19 @@
+package me.cxis.gof.template_method_pattern;
+
+public class ConcreteClass extends AbstractClass {
+
+    @Override
+    protected void primitiveOperation3() {
+
+    }
+
+    @Override
+    protected void primitiveOperation2() {
+
+    }
+
+    @Override
+    protected void primitiveOperation1() {
+
+    }
+}
