7942e9a1aec9396f22d6809287a066e814fb0aa6
Pullock <pullocked@gmail.com>
2018-12-11 17:46:34 +0800

    ServiceLoader测试

---
+++ java/src/main/java/me/cxis/spi/service/impl/Driver.java
@@ -0,0 +1,51 @@
+package me.cxis.spi.service.impl;
+
+import java.sql.Connection;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class Driver implements java.sql.Driver {
+
+    static {
+        System.out.println("初始化mysql驱动成功");
+    }
+
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        System.out.println(url);
+        return null;
+    }
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        return new DriverPropertyInfo[0];
+    }
+
+    @Override
+    public int getMajorVersion() {
+        return 0;
+    }
+
+    @Override
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        return false;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+}
