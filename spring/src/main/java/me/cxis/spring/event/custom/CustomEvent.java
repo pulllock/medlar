f379565328f877e84131ff59ca514e08a8bd8ddf
Pullock <pullocked@gmail.com>
2019-03-03 18:59:54 +0800

    spring event

---
+++ spring/src/main/java/me/cxis/spring/event/custom/CustomEvent.java
@@ -0,0 +1,10 @@
+package me.cxis.spring.event.custom;
+
+import org.springframework.context.ApplicationEvent;
+
+public class CustomEvent extends ApplicationEvent {
+
+    public CustomEvent(Object source) {
+        super(source);
+    }
+}
