9ea480d03e4f16ff2ce6f5d6cac3e6349e8405d4
dachengxi <dachengdaxi@gmail.com>
2020-02-13 22:02:06 +0800

    double linked list

--- java/src/main/java/me/cxis/algorithms/list/doublelinkedlist/DoubleLinkedList.java
+++ java/src/main/java/me/cxis/algorithms/list/doublelinkedlist/DoubleLinkedList.java
@@ -7,21 +7,67 @@ public class DoubleLinkedList {
     private int size;
 
     public void insert(int index, int data) {
-        // get element
-
-        // insert
+        if (size == 0) {
+            Node newNode = new Node(data);
+            head.setNext(newNode);
+            newNode.setPrev(head);
+            size++;
+        } else {
+            // get element
+            Node p = getElement(index);
+            // insert
+            Node newNode = new Node(data);
+            newNode.setPrev(p.getPrev());
+            p.getPrev().setNext(newNode);
+            newNode.setNext(p);
+            p.setPrev(newNode);
+            size++;
+        }
     }
 
+    public void delete(int index) {
+        Node p = getElement(index);
+        p.getPrev().setNext(p.getNext());
+        Node next = p.getNext();
+        if (next != null) {
+            next.setPrev(p.getPrev());
+        }
+        size--;
+    }
 
+    private Node getElement(int index) {
+        Node p = head.getNext();
+        int i = 1;
+        while (p != null && i < index) {
+            p = p.getNext();
+            i++;
+        }
+        return p;
+    }
+
+    private void list() {
+        Node p = head;
+        int i = 0;
+        while (p != null && i < size) {
+            p = p.getNext();
+            i++;
+            System.out.println(p.getData());
+        }
+    }
 
     public static void main(String[] args) {
         DoubleLinkedList list = new DoubleLinkedList();
         list.insert(1, 10);
-        System.out.println(list.head);
-        list.insert(2, 20);
-        System.out.println(list.head);
+        list.list();
+        System.out.println("-----");
+        list.insert(1, 20);
+        list.list();
+        System.out.println("-----");
         list.insert(2, 30);
-        System.out.println(list.head);
+        list.list();
+        System.out.println("-----");
+        list.delete(2);
+        list.list();
     }
 
     private class Node {
