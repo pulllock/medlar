4611f7321a40790b92aef6d88f410798b12b400b
dachengxi <dachengdaxi@gmail.com>
2020-03-01 23:31:49 +0800

    dynamic programming maximum product subarray

---
+++ java/src/main/java/me/cxis/algorithms/dynamic_programming/maximun_product_subarray/MaximumProductSubarray.java
@@ -0,0 +1,18 @@
+package me.cxis.algorithms.dynamic_programming.maximun_product_subarray;
+
+/**
+ * 给定数组a[0]...a[n-1]
+ * 找到最长的连续子序列i,i+1,i+2...j，使a[i] * a[i+1] * ... * a[j]最大
+ *
+ * [2, 3, -2, 4] -> 6 (2 * 3 = 6)
+ *
+ * 最值型动态规划
+ *
+ * 问题：
+ *
+ * 确定状态：
+ *
+ */
+public class MaximumProductSubarray {
+    // TODO
+}
