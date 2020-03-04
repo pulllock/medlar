500f1396a095aee94725cc24bb8fe33ee8bb9af5
dachengxi <dachengdaxi@gmail.com>
2020-03-04 22:54:26 +0800

    dynamic programming counting bits

---
+++ java/src/main/java/me/cxis/algorithms/dynamic_programming/counting_bits/CountingBits.java
@@ -0,0 +1,21 @@
+package me.cxis.algorithms.dynamic_programming.counting_bits;
+
+/**
+ * 给定N，输出0,1,2....,N的每个数的二进制表示中含有1的个数
+ *
+ * 输入：5
+ * 输出：[0,1,1,2,1,2]
+ *
+ * 0:0
+ * 1:1
+ * 2:10
+ * 3:11
+ * 4:100
+ * 5:101
+ *
+ *
+ */
+public class CountingBits {
+
+    // TODO
+}
