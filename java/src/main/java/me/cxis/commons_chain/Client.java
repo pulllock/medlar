1b5d9ffa2483b97636ef898e3dcea97cd94daa68
Pullock <pullocked@gmail.com>
2020-04-09 21:21:52 +0800

    commons chain

--- java/src/main/java/me/cxis/commons_chain/Client.java
+++ java/src/main/java/me/cxis/commons_chain/Client.java
@@ -1,5 +1,7 @@
 package me.cxis.commons_chain;
 
+import com.alibaba.fastjson.JSON;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
@@ -7,12 +9,16 @@ public class Client {
         CommonContext commonContext = new CommonContext();
         commonContext.put("request", "this hack");
         System.out.println(chains.execute(commonContext));
-        // System.out.println(JSON.tojsonString(commonContext));
+        System.out.println(JSON.toJSONString(commonContext));
 
+        commonContext = new CommonContext();
         commonContext.put("request", "this error");
         System.out.println(chains.execute(commonContext));
+        System.out.println(JSON.toJSONString(commonContext));
 
+        commonContext = new CommonContext();
         commonContext.put("request", "this is normal request");
         System.out.println(chains.execute(commonContext));
+        System.out.println(JSON.toJSONString(commonContext));
     }
 }
