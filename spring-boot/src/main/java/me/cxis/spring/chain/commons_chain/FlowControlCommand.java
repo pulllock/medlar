683ed603cea61f69e979625440ec0eb8f58088c3
Pullock <pullocked@gmail.com>
2020-04-12 21:36:10 +0800

    commons chain spring

--- spring-boot/src/main/java/me/cxis/spring/chain/commons_chain/FlowControlCommand.java
+++ spring-boot/src/main/java/me/cxis/spring/chain/commons_chain/FlowControlCommand.java
@@ -1,8 +1,5 @@
 package me.cxis.spring.chain.commons_chain;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class FlowControlCommand extends AbstractCommand {
 
     @Override
