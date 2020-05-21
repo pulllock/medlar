48f9d8c3a4c690ec41404b0d4545737fc3e3a039
Pullock <pullocked@gmail.com>
2020-05-22 00:28:16 +0800

    delay queue

---
+++ java/src/main/java/me/cxis/queue/delayqueue/example1/Client.java
@@ -0,0 +1,16 @@
+package me.cxis.queue.delayqueue.example1;
+
+public class Client {
+
+    public static void main(String[] args) throws InterruptedException {
+        SmsSender smsSender = new SmsSender();
+
+        long createTime = System.currentTimeMillis();
+        smsSender.sendDelaySms("短信1", createTime, 10 * 1000);
+
+        smsSender.sendDelaySms("短信2", createTime, 5 * 1000);
+
+        smsSender.sendDelaySms("短信3", createTime, 30 * 1000);
+
+    }
+}
