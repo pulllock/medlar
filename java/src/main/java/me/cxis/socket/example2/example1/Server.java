b7a60d48914d68eefa659ed8a8e380953e79c44d
dachengxi <dachengdaxi@gmail.com>
2020-05-26 00:56:07 +0800

    socket

---
+++ java/src/main/java/me/cxis/socket/example2/example1/Server.java
@@ -0,0 +1,42 @@
+package me.cxis.socket.example2.example1;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(8888);
+                    while (true) {
+                        Socket socket = serverSocket.accept();
+                        handleSocket(socket);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private static void handleSocket(Socket socket) {
+        try {
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            int reqRes = inputStream.readInt();
+            int type = inputStream.readInt();
+            int size = inputStream.readInt();
+            byte[] data = new byte[size];
+            inputStream.readFully(data);
+
+            System.out.println(String.format("reqRes: %s, type: %s, data: %s", reqRes, type, new String(data)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
