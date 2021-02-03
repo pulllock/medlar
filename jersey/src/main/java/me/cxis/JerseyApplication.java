4003f25bbadf67a8cdd004edbc106d519d0794a9
Pullock <pullocked@gmail.com>
2021-02-03 17:39:01 +0800

    remove jersy module

--- jersey/src/main/java/me/cxis/JerseyApplication.java
+++
@@ -1,9 +0,0 @@
-package me.cxis;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("")
-public class JerseyApplication extends ResourceConfig {
-}
