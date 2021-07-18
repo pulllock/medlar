c1958fcd0664add8f70b9e7a4523999b8ffec0ac
dachengxi <dachengdaxi@gmail.com>
2021-07-18 23:59:21 +0800

    mybatis test

--- spring-boot-mybatis/src/test/java/me/cxis/mybatis/origin/MyBatisTest.java
+++ spring-boot-mybatis/src/test/java/me/cxis/mybatis/origin/MyBatisTest.java
@@ -3,10 +3,13 @@ package me.cxis.mybatis.origin;
 import com.alibaba.fastjson.JSON;
 import me.cxis.mybatis.dao.mapper.ApiDOMapper;
 import me.cxis.mybatis.dao.model.ApiDO;
+import org.apache.ibatis.builder.xml.XMLConfigBuilder;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -30,6 +33,20 @@ public class MyBatisTest {
         String resource = "mybatis/origin/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            ApiDOMapper apiDOMapper = session.getMapper(ApiDOMapper.class);
+            ApiDO apiDO = apiDOMapper.selectByPrimaryKey( 1L);
+            System.out.println(JSON.toJSONString(apiDO));
+        }
+    }
+
+    @Test
+    public void test2() throws IOException {
+        String resource = "mybatis/origin/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder(inputStream);
+        Configuration configuration = xmlConfigBuilder.parse();
+        SqlSessionFactory sqlSessionFactory = new DefaultSqlSessionFactory(configuration);
         try (SqlSession session = sqlSessionFactory.openSession()) {
             ApiDOMapper apiDOMapper = session.getMapper(ApiDOMapper.class);
             ApiDO apiDO = apiDOMapper.selectByPrimaryKey( 1L);
