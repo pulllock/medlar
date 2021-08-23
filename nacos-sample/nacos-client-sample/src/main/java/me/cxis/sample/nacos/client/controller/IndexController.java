d57f214beae0ce5eee83f6a100d8f0d136eb8913
Pullock <pullocked@gmail.com>
2021-08-24 01:34:32 +0800

    nacos sample

---
+++ nacos-sample/nacos-client-sample/src/main/java/me/cxis/sample/nacos/client/controller/IndexController.java
@@ -0,0 +1,24 @@
+package me.cxis.sample.nacos.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/client/index")
+public class IndexController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${user.service.url}")
+    private String userServiceUrl;
+
+    @RequestMapping("/info")
+    public String indexInfo() {
+        return restTemplate.getForObject(userServiceUrl + "/user/queryNamePrefix", String.class);
+    }
+}
---
+++ nacos-sample/nacos-user-sample/src/main/java/me/cxis/sample/nacos/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package me.cxis.sample.nacos.user.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Value("${user.name.prefix}")
+    private String userNamePrefix;
+
+    @RequestMapping("/queryNamePrefix")
+    public String queryNamePrefix() {
+        return userNamePrefix;
+    }
+}
