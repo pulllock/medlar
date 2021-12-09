a09e41c0051c340d5a8703ca3816deffa4af5d78
dachengxi <dachengdaxi@gmail.com>
2021-12-09 12:59:41 +0800

    dubbo grpc

--- dubbo3-sample/dubbo3-consumer-sample/dubbo3-consumer-sample-core/src/main/java/me/cxis/sample/dubbo3/consumer/core/ApplicationMain.java
+++ dubbo3-sample/dubbo3-consumer-sample/dubbo3-consumer-sample-core/src/main/java/me/cxis/sample/dubbo3/consumer/core/ApplicationMain.java
@@ -1,8 +1,10 @@
 package me.cxis.sample.dubbo3.consumer.core;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
 @SpringBootApplication
 public class ApplicationMain {
 
