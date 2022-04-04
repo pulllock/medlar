13661dfcbf7603c7dbf90f490db3c5393da6f33e
Pullock <pullocked@gmail.com>
2022-04-04 18:22:33 +0800

    maven surefire plugin

---
+++ spring-boot-maven-plugin/spring-boot-maven-plugin-site/src/test/java/me/cxis/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package me.cxis.controller;
+
+import me.cxis.IntegrationBase;
+import me.cxis.model.User;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+
+public class UserControllerTest extends IntegrationBase {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserControllerTest.class);
+
+    @Resource
+    private UserController userController;
+
+    @Test
+    public void testQueryByUserId() {
+        Long userId = 1L;
+        User result = userController.queryByUserId(userId);
+        LOGGER.info("User: {}", result);
+    }
+
+    @Test
+    public void testQueryByUserName() {
+        String userName = "name";
+        User result = userController.queryByUserName(userName);
+        LOGGER.info("User: {}", result);
+    }
+}
