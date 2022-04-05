f1e7d3d22ec8873b725048509df1f1269cf5050e
Pullock <pullocked@gmail.com>
2022-04-05 22:41:36 +0800

    add maven sample

---
+++ spring-boot-maven/spring-boot-maven-sample/src/test/java/me/cxis/maven/sample/controller/UserControllerTest.java
@@ -0,0 +1,31 @@
+package me.cxis.maven.sample.controller;
+
+import me.cxis.maven.sample.model.User;
+import me.cxis.maven.sample.AbstractIntegrationTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+
+public class UserControllerTest extends AbstractIntegrationTest {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserControllerTest.class);
+
+    @Resource
+    private UserController userController;
+
+    @Test
+    public void testQueryByUserId() {
+        Long userId = 1L;
+        User result = userController.queryByUserId(userId);
+        LOGGER.info("User: {}", result);
+    }
+
+    @Test
+    public void testQueryByUserName() {
+        String userName = "name";
+        User result = userController.queryByUserName(userName);
+        LOGGER.info("User: {}", result);
+    }
+}
