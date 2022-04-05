fdd7de8153aa8a106315b9d376de50cad74587e3
dachengxi <dachengdaxi@gmail.com>
2022-04-05 22:41:36 +0800

    add maven sample

---
+++ spring-boot-maven/spring-boot-maven-sample/src/main/java/me/cxis/maven/sample/SpringBootMavenSampleApplication.java
@@ -0,0 +1,13 @@
+package me.cxis.maven.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenSampleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-maven/spring-boot-maven-sample/src/main/java/me/cxis/maven/sample/controller/UserController.java
@@ -0,0 +1,30 @@
+package me.cxis.maven.sample.controller;
+
+import me.cxis.maven.sample.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/queryByUserId")
+    public User queryByUserId(@RequestParam Long userId) {
+        User user = new User();
+        user.setUserId(1L);
+        user.setUserName("my name");
+        user.setSex(1);
+        return user;
+    }
+
+    @GetMapping("/queryByUserName")
+    public User queryByUserName(@RequestParam String userName) {
+        User user = new User();
+        user.setUserId(1L);
+        user.setUserName("my name");
+        user.setSex(1);
+        return user;
+    }
+}
