e7c808392f48645f9c3891a371dbfce32d02d4e1
Pullock <pullocked@gmail.com>
2022-04-05 22:46:27 +0800

    maven surefire plugin

---
+++ spring-boot-maven/spring-boot-maven-plugin/spring-boot-maven-plugin-site/src/main/java/me/cxis/maven/plugin/SpringBootMavenPluginSurefireApplication.java
@@ -0,0 +1,13 @@
+package me.cxis.maven.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenPluginSurefireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenPluginSurefireApplication.class, args);
+    }
+
+}
---
+++ spring-boot-maven/spring-boot-maven-plugin/spring-boot-maven-plugin-surefire-report/src/test/java/me/cxis/maven/plugin/AbstractIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.cxis.maven.plugin;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class AbstractIntegrationTest {
+}
