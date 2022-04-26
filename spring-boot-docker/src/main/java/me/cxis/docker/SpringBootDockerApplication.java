18df960654906e982213493a8eaf3d922427520d
dachengxi <dachengdaxi@gmail.com>
2022-04-26 16:24:45 +0800

    docker

---
+++ spring-boot-docker/src/main/java/me/cxis/docker/SpringBootDockerApplication.java
@@ -0,0 +1,12 @@
+package me.cxis.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+}
