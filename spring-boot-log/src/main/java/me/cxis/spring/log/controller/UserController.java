8d31c441b21af50ad017226c845d4a96429a0804
dachengxi <dachengdaxi@gmail.com>
2022-07-01 19:01:39 +0800

    log

---
+++ spring-boot-log/src/main/java/me/cxis/spring/log/controller/UserController.java
@@ -0,0 +1,23 @@
+package me.cxis.spring.log.controller;
+
+import me.cxis.spring.log.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getById")
+    public User getById(@RequestParam Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("xiao hong");
+        user.setAge(20);
+        return user;
+    }
+
+    @PostMapping("/create")
+    public Boolean create(@RequestBody User user) {
+        return true;
+    }
+}
