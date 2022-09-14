5d561e7349d48d21b5f1f939eb89488a2326767c
Pullock <pullocked@gmail.com>
2022-09-14 16:44:55 +0800

    i18n

---
+++ spring-boot-i18n/spring-boot-i18n-custom/src/main/java/me/cxis/i18n/SpringBootI18nCustomApplication.java
@@ -0,0 +1,12 @@
+package me.cxis.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootI18nCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootI18nCustomApplication.class, args);
+    }
+}
