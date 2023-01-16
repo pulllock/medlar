e05b7eb55ac2d8e956a3e41e319e30a115d8e70a
Pullock <pullocked@gmail.com>
2023-01-17 00:11:19 +0800

    gateway rate limit filter

--- spring-cloud-sample/spring-cloud-gateway/spring-cloud-gateway-server/src/main/java/me/cxis/sample/cloud/gateway/server/config/GatewayFilterConfig.java
+++ spring-cloud-sample/spring-cloud-gateway/spring-cloud-gateway-server/src/main/java/me/cxis/sample/cloud/gateway/server/config/GatewayFilterConfig.java
@@ -1,6 +1,7 @@
 package me.cxis.sample.cloud.gateway.server.config;
 
 import me.cxis.sample.cloud.gateway.server.filters.AuthenticationGatewayFilter;
+import me.cxis.sample.cloud.gateway.server.filters.IpKeyResolver;
 import me.cxis.sample.cloud.gateway.server.filters.LoggingGatewayFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +20,10 @@ public class GatewayFilterConfig {
     @Bean
     public AuthenticationGatewayFilter authenticationGatewayFilter() {
         return new AuthenticationGatewayFilter();
+    }
+
+    @Bean
+    public IpKeyResolver ipKeyResolver() {
+        return new IpKeyResolver();
     }
 }
