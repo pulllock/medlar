70f28464f3fef25f8f6599fd040a195e5d9291c2
Pullock <pullocked@gmail.com>
2023-02-17 01:04:43 +0800

    grpc springboot 3

--- spring-boot-grpc/grpc-nacos-client-sample/grpc-nacos-client-sample-core/src/main/java/me/cxis/sample/grpc/nacos/client/core/controller/UserController.java
+++ spring-boot-grpc/grpc-nacos-client-sample/grpc-nacos-client-sample-core/src/main/java/me/cxis/sample/grpc/nacos/client/core/controller/UserController.java
@@ -1,12 +1,12 @@
 package me.cxis.sample.grpc.nacos.client.core.controller;
 
+import jakarta.annotation.Resource;
 import me.cxis.sample.grpc.nacos.client.core.proxy.UserServiceProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/user")
