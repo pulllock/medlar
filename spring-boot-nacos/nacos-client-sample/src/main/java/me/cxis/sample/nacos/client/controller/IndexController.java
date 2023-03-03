446ee0e2c09a6865fd3679d366a7039fd05e5856
Pullock <pullocked@gmail.com>
2023-03-03 10:20:42 +0800

    nacos

--- spring-boot-nacos/nacos-client-sample/src/main/java/me/cxis/sample/nacos/client/controller/IndexController.java
+++ spring-boot-nacos/nacos-client-sample/src/main/java/me/cxis/sample/nacos/client/controller/IndexController.java
@@ -1,11 +1,11 @@
 package me.cxis.sample.nacos.client.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/client/index")
