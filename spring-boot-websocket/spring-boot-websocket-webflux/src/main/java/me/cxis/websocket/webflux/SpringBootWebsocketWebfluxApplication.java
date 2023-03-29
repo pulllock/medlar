8c464b239e2aaa7a51e318111e7ab7ac4bc9df8a
Pullock <pullocked@gmail.com>
2023-03-29 23:30:01 +0800

    websocket webflux

---
+++ spring-boot-websocket/spring-boot-websocket-webflux/src/main/java/me/cxis/websocket/webflux/SpringBootWebsocketWebfluxApplication.java
@@ -0,0 +1,13 @@
+package me.cxis.websocket.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebsocketWebfluxApplication.class, args);
+    }
+
+}
---
+++ spring-boot-websocket/spring-boot-websocket-webflux/src/main/java/me/cxis/websocket/webflux/server/WebSocketServerHandler.java
@@ -0,0 +1,25 @@
+package me.cxis.websocket.webflux.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+public class WebSocketServerHandler implements WebSocketHandler {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(WebSocketServerHandler.class);
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        // TODO
+        return session.send(
+                session.receive()
+                        .map(msg -> {
+                            String message = String.format("这是来自客户端：%s的消息，内容为：%s", session.getId(), msg.getPayloadAsText());
+                            return session.textMessage(message);
+                        })
+        );
+    }
+
+}
