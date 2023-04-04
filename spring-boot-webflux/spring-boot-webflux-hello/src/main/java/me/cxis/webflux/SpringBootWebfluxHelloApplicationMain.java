b472ef5d2b885109753335fd1e40b8e3ccc31f9c
Pullock <pullocked@gmail.com>
2023-04-04 21:26:39 +0800

    webflux

---
+++ spring-boot-webflux/spring-boot-webflux-hello/src/main/java/me/cxis/webflux/SpringBootWebfluxHelloApplicationMain.java
@@ -0,0 +1,12 @@
+package me.cxis.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxHelloApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxHelloApplicationMain.class, args);
+    }
+}
