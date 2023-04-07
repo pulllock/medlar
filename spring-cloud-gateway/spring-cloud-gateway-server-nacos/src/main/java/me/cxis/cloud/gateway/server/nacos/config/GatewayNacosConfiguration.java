b13c97884b9405c37e9119c8c07c5ea046136803
Pullock <pullocked@gmail.com>
2023-04-07 16:30:36 +0800

    fix error

--- spring-cloud-gateway/spring-cloud-gateway-server-nacos/src/main/java/me/cxis/cloud/gateway/server/nacos/config/GatewayNacosConfiguration.java
+++ spring-cloud-gateway/spring-cloud-gateway-server-nacos/src/main/java/me/cxis/cloud/gateway/server/nacos/config/GatewayNacosConfiguration.java
@@ -1,11 +1,11 @@
 package me.cxis.cloud.gateway.server.nacos.config;
 
 import com.alibaba.cloud.nacos.NacosConfigManager;
+import jakarta.annotation.Resource;
 import me.cxis.cloud.gateway.server.nacos.route.NacosRouteDefinitionRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.Resource;
 
 /**
  * Nacos动态路由配置
