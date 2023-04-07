2ea93f80a7062dbb25f41aded64a62db832144f0
Pullock <pullocked@gmail.com>
2023-04-07 21:57:00 +0800

    openfeign

---
+++ spring-cloud-open-feign/spring-cloud-open-feign-client/spring-cloud-open-feign-client-core/src/main/java/me/cxis/cloud/open/feign/client/proxy/UserServiceProxy.java
@@ -0,0 +1,26 @@
+package me.cxis.cloud.open.feign.client.proxy;
+
+import jakarta.annotation.Resource;
+import me.cxis.cloud.open.feign.server.api.model.User;
+import me.cxis.cloud.open.feign.server.api.model.common.Result;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceProxy {
+
+    @Resource
+    private UserService userService;
+
+    public User queryUserById(Long userId) {
+        try {
+            Result<User> result = userService.queryUserById(userId);
+            if (result == null || !result.isSuccess()) {
+                return null;
+            }
+            return result.getData();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
