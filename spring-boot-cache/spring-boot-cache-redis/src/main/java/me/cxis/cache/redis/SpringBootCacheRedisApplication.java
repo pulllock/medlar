b2e6d77a55aa2671dd24f3e6f26783a399039044
Pullock <pullocked@gmail.com>
2023-04-08 11:43:42 +0800

    spring cache redis

--- spring-boot-cache/spring-boot-cache-redis/src/main/java/me/cxis/cache/redis/SpringBootCacheRedisApplication.java
+++ spring-boot-cache/spring-boot-cache-redis/src/main/java/me/cxis/cache/redis/SpringBootCacheRedisApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCacheRedisApplicationMain {
+public class SpringBootCacheRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootCacheRedisApplicationMain.class, args);
+        SpringApplication.run(SpringBootCacheRedisApplication.class, args);
     }
 }
