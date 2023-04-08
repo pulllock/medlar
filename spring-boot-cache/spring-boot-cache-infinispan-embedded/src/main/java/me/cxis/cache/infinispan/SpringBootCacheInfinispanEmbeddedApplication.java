8f28f32a922ea15583266b27346e55c144734fd3
Pullock <pullocked@gmail.com>
2023-04-09 00:17:47 +0800

    spring cache infinispan embedded

---
+++ spring-boot-cache/spring-boot-cache-infinispan-embedded/src/main/java/me/cxis/cache/infinispan/SpringBootCacheInfinispanEmbeddedApplication.java
@@ -0,0 +1,12 @@
+package me.cxis.cache.infinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheInfinispanEmbeddedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheInfinispanEmbeddedApplication.class, args);
+    }
+}
