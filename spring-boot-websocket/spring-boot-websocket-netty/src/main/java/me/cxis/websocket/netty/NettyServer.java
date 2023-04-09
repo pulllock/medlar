d574acf33c4a242ee2f35c8b24c826396f86b4aa
Pullock <pullocked@gmail.com>
2023-04-09 22:17:28 +0800

    websocket

--- spring-boot-websocket/spring-boot-websocket-netty/src/main/java/me/cxis/websocket/netty/NettyServer.java
+++ spring-boot-websocket/spring-boot-websocket-netty/src/main/java/me/cxis/websocket/netty/NettyServer.java
@@ -12,6 +12,7 @@ import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.serialization.ObjectEncoder;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,9 +23,15 @@ import java.net.InetSocketAddress;
 @Component
 public class NettyServer {
 
+    /**
+     * 端口
+     */
     @Value("${netty.websocket.port:8989}")
     private int port;
 
+    /**
+     * 路径
+     */
     @Value("${netty.websocket.path:/websocket}")
     private String path;
 
@@ -35,23 +42,42 @@ public class NettyServer {
 
     private EventLoopGroup workGroup;
 
+    @PostConstruct
+    public void init() {
+        new Thread(
+                () -> {
+                    try {
+                        start();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).start();
+    }
+
     private void start() throws InterruptedException {
         bossGroup = new NioEventLoopGroup();
         workGroup = new NioEventLoopGroup();
 
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(bossGroup, workGroup)
+                // NIO
                 .channel(NioServerSocketChannel.class)
+                // 监听端口
                 .localAddress(new InetSocketAddress(port))
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel socketChannel) throws Exception {
                         socketChannel.pipeline()
+                                // http编解码器
                                 .addLast(new HttpServerCodec())
                                 .addLast(new ObjectEncoder())
                                 .addLast(new ChunkedWriteHandler())
+                                // 分段聚合
                                 .addLast(new HttpObjectAggregator(8192))
+                                // websocket协议
                                 .addLast(new WebSocketServerProtocolHandler(path, "WebSocket", true, 65536 * 10))
+                                // 自定义业务处理器
                                 .addLast(webSocketServerHandler);
                     }
                 });
--- spring-boot-websocket/spring-boot-websocket-netty/src/main/java/me/cxis/websocket/netty/WebSocketServerHandler.java
+++ spring-boot-websocket/spring-boot-websocket-netty/src/main/java/me/cxis/websocket/netty/WebSocketServerHandler.java
@@ -3,14 +3,24 @@ package me.cxis.websocket.netty;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(WebSocketServerHandler.class);
+
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
-        // TODO
+        LOGGER.info("收到消息，channel: {}, message: {}", channelHandlerContext.channel().id().asLongText(), textWebSocketFrame.text());
+
+        // 获取userId
+        String userId = "mock_user_id";
+
+        // 放到
+        
     }
 
     @Override
@@ -21,8 +31,9 @@ public class WebSocketServerHandler extends SimpleChannelInboundHandler<TextWebS
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        // TODO
-        super.handlerAdded(ctx);
+        LOGGER.info("handler added, channel: {}", ctx.channel().id().asLongText());
+        // 添加到channelGroup中
+        NettyConfig.getChannelGroup().add(ctx.channel());
     }
 
     @Override
