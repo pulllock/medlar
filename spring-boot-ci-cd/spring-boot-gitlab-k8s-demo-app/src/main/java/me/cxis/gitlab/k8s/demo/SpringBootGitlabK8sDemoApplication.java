c94d27e02de278867da61a253dce242199481100
Pullock <pullocked@gmail.com>
2023-04-25 22:13:14 +0800

    gitlab ci k8s

---
+++ spring-boot-ci-cd/spring-boot-gitlab-k8s-demo-app/src/main/java/me/cxis/gitlab/k8s/demo/SpringBootGitlabK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package me.cxis.gitlab.k8s.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitlabK8sDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGitlabK8sDemoApplication.class, args);
+    }
+
+}
