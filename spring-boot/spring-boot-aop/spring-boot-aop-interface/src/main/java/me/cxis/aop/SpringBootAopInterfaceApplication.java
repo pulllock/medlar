19a933e9826bb0ea893ef27ab6c600688511f804
Pullock <pullocked@gmail.com>
2023-10-17 18:36:21 +0800

    spring aop

---
+++ spring-boot/spring-boot-aop/spring-boot-aop-interface/src/main/java/me/cxis/aop/SpringBootAopInterfaceApplication.java
@@ -0,0 +1,12 @@
+package me.cxis.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAopInterfaceApplication.class, args);
+    }
+}
---
+++ spring-boot/spring-boot-aop/spring-boot-aop-interface/src/main/java/me/cxis/aop/service/UserService.java
@@ -0,0 +1,8 @@
+package me.cxis.aop.service;
+
+import me.cxis.aop.model.User;
+
+public interface UserService {
+
+    User getById(Long id);
+}
